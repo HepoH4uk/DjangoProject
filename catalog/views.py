7c636a106d6f67903e6b58e9e5e0bf940a3eb3ff
AlTim <aleks00_777@mail.ru>
2025-05-16 02:50:21 +0300

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    if request.method != "POST":
+        return render(request, 'contacts.html')
+    elif request.method == "POST":
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        return HttpResponse(f"{name}, благодарим Вас за обращение!")
+    return render(request,'catalog/templates/contacts.html')
