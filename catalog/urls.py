164ee5f771dd2c886e231945ffb6a71a078b48b1
AlTim <aleks00_777@mail.ru>
2025-06-10 12:07:19 +0300

    update urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, products_details
+from catalog.views import HomeView, ContactsView, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 
 app_name = CatalogConfig.name
 
+
 urlpatterns = [
-    path('home/', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('products_list/', products_list, name='products_list'),
-    path('products/<int:pk>', products_details, name='products_details'),
+    path("", HomeView.as_view(), name="home"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path('products_list/', ProductListView.as_view(), name='products_list'),
+    path('products_details/<int:pk>/', ProductDetailView.as_view(), name='products_details'),
+    path('products_create/', ProductCreateView.as_view(), name='products_create'),
+    path('<int:pk>/products_update/', ProductUpdateView.as_view(), name='products_update'),
+    path('<int:pk>/product_delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
-
-
-
