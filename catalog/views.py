f2932088d99f68d134b1f7875a2da5db7f012022
AlTim <aleks00_777@mail.ru>
2025-06-11 14:45:59 +0300

    update views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+
+from catalog.form import ProductForm
 from catalog.models import Products
 
 
@@ -20,17 +22,24 @@ class ProductDetailView(DetailView):
     model = Products
     template_name = 'products_details.html'
 
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        # увеличиваем счетчик просмотров при каждом просмотре
+        obj.views_counter += 1
+        obj.save(update_fields=['views_counter'])
+        return obj
+
 
 class ProductCreateView(CreateView):
     model = Products
-    fields = ("name","description","image","category","purchase_price")
+    form_class = ProductForm
     success_url = reverse_lazy('products:products_list')
     template_name = 'product_form.html'
 
 
 class ProductUpdateView(UpdateView):
     model = Products
-    fields = ("name","description","image","category","purchase_price")
+    form_class = ProductForm
     success_url = reverse_lazy('products:products_list')
     template_name = 'product_form.html'
 
