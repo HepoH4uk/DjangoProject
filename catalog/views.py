a01fc249546b5bbd5cc99abc68edf6f9db6eb37f
AlTim <aleks00_777@mail.ru>
2025-06-24 11:46:48 +0300

    fix catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -38,6 +38,13 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('products:products_list')
     template_name = 'product_form.html'
 
+    def form_valid(self, form):
+        product = form.save()
+        user = self.request.user
+        product.owner = user
+        product.save()
+        return super().form_valid(form)
+
 
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Products
