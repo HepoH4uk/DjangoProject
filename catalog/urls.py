a86fc2b13ea6989f4b39582bb877bbd10aa562e3
AlTim <aleks00_777@mail.ru>
2025-06-27 21:43:29 +0300

    fix urls.py

--- catalog/urls.py
+++ catalog/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('products_create/', ProductCreateView.as_view(), name='products_create'),
     path('<int:pk>/products_update/', ProductUpdateView.as_view(), name='products_update'),
     path('<int:pk>/product_delete/', ProductDeleteView.as_view(), name='product_delete'),
-    path('product/category/<int:pk>', ProductsByCategoryDetailView.as_view(), name='products_category'),
+    path('product/category/<int:pk>/', ProductsByCategoryDetailView.as_view(), name='products_category'),
     path('product/category/', CategoryListView.as_view(), name='category_list'),
 ]
